eb4afdfb38591e6a9ec3cab84ff11763093248d0
Isaque Ramos Chaves <isaque.chaves@escolaconquer.com.br>
2023-12-17 15:32:57 -0300

    feat: add prettier config and Global styled component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
-  return (
-    <h1>DentistApp</h1>
-  )
+    return (
+        <h1>a s</h1>
+    )
 }
---
+++ src/app/components/globals.style.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const Global = styled.html`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: 0;
+}
+`;
